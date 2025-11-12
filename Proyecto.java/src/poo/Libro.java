b1ea0326c3d2c28f7f103a7b7d6d721027f36779
Juan Francisco González Gómez <gonzalezjf@pm.me>
2025-11-12 14:52:00 +0100

    libro

---
+++ Proyecto.java/src/poo/Libro.java
@@ -0,0 +1,34 @@
+package poo;
+
+public class Libro {
+    
+    String titulo;
+    String ISBN;
+    String editorial;
+    String idioma;
+    int numPag;
+    int anio;
+    boolean dispreserva;
+
+    public Libro(String titulo,String ISBN, String editorial, String idioma, int numPag, int anio, boolean dispreserva) {
+
+        this.titulo= titulo;
+        this.ISBN =  ISBN;
+        this.editorial =  editorial;
+        this.idioma = idioma;
+        this.numPag = numPag;
+        this.anio = anio;
+        this.dispreserva = dispreserva;
+
+    }
+
+    public void setTitulo ( String titulo) {
+        this.titulo = titulo;
+    }
+    public void getTitulo (){
+        return this.titulo;
+    }
+
+
+
+}
